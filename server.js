577f9b1ec3cd1bd70a239207b08a12b4388b2284
Alec Luna <alecluna@csus.edu>
2019-01-08 19:51:48 -0800

    added refresh_token for our access token

--- server.js
+++ server.js
@@ -1,31 +1,71 @@
 let express = require('express')
 let request = require('request')
-let querystring = require('querystring')
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-let app = express()
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+
+var app = express()
+
+app.use(cors())
+  .use(cookieParser());
 
 let redirect_uri = 
   process.env.REDIRECT_URI || 
   'http://localhost:8888/callback'
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+
+  // application requests authorization
+  var scope = 'user-read-private user-read-email';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
-      redirect_uri
-    }))
-})
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
 
+//redirect link that retrievs a token 
 app.get('/callback', function(req, res) {
-  let code = req.query.code || null
-  let authOptions = {
+  var uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(uri + querystring.stringify({ error: "state_mismatch" }));
+  } 
+  else {
+  res.clearCookie(stateKey);
+  var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
       redirect_uri,
-      grant_type: 'client_credentials'
+      grant_type: 'authorization_code'
     },
     headers: {
       'Authorization': 'Basic ' + (new Buffer(
@@ -33,13 +73,54 @@ app.get('/callback', function(req, res) {
       ).toString('base64'))
     },
     json: true
-  }
+  }}
   request.post(authOptions, function(error, response, body) {
-    var access_token = body.access_token
+
+    if (!error && response.statusCode === 200) {
+
+      var access_token = body.access_token,
+        refresh_token = body.refresh_token;
+
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
-  })
+
+      res.redirect(uri +
+        querystring.stringify({
+          access_token: access_token,
+          refresh_token: refresh_token
+        }));
+    } else {
+      res.redirect(uri +
+        querystring.stringify({
+          error: 'invalid_token'
+        }));
+    }
+  });
 })
+
+app.get('/refresh_token', function (req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
 
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
