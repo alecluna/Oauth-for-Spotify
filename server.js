1207651c645882b355a4c0d4210747f816af377b
Alec Luna <alecluna@csus.edu>
2019-04-20 14:58:36 -0700

    add refresh_token endpoint, update with ES6 syntax

--- server.js
+++ server.js
@@ -81,17 +81,10 @@ app.get("/callback", function(req, res) {
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      let access_token = body.access_token,
-        refresh_token = body.refresh_token;
-
+      let access_token = body.access_token;
       let uri = process.env.FRONTEND_URI || "http://localhost:3000";
-      res.redirect(
-        uri +
-          "?access_token=" +
-          access_token +
-          "&refresh_token=" +
-          refresh_token
-      );
+
+      res.redirect(`${uri}?access_token=${access_token}`);
     } else {
       res.redirect(
         uri +
@@ -99,6 +92,33 @@ app.get("/callback", function(req, res) {
             error: "invalid_token"
           })
       );
+    }
+  });
+});
+
+//refresh token endpoint
+app.get("/refresh_token", function(req, res) {
+  let refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64")
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let access_token = body.access_token;
+      res.send({
+        access_token: access_token
+      });
     }
   });
 });
